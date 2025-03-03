a557a137d614b467cfea9086b1a8d83c0ac9713d
Gang Wang <gang.wang@totalsynergy.com>
2025-03-04 10:46:43 +1100

    update

--- 05-axios-tutorial/src/App.js
+++ 05-axios-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import Title from './components/Title';
+import Interceptors from './final/examples/6-interceptors'
 function App() {
   return (
     <main>
       <Title />
+      <Interceptors />
     </main>
-  );
+  )
 }
 
 export default App;
