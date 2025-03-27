873b147bcaed24257d923dd87fe3045a16ba4f6b
Gang Wang <gang.wang@totalsynergy.com>
2025-03-28 10:52:58 +1100

    Update App.jsx

--- 03-advanced-react/src/App.jsx
+++ 03-advanced-react/src/App.jsx
@@ -1,9 +1,11 @@
+import ReducerBasics  from './tutorial/10-useReducer/final/01-useReducer'
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <h2>Advanced React</h2>
+      <ReducerBasics />
     </div>
-  );
+  )
 }
 
 export default App;
